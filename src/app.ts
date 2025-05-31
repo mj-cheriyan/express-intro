db2bc94f6c33f131f3aca882275faf2be17f13db
MJ Cheriyan <midhuncheriyan@gmail.com>
2025-05-31 09:23:46 +0100

    Error Handling

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,11 @@ app.use(express.json());
 app.use('/admin', adminRoutes);
 app.use('/public', publicRoutes);
 
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
 app.listen(8080, () => {
   console.log('Server running on http://localhost:8080');
 });
